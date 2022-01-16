b1b8d4b37896a11fcf8c509eeca9544c8bac0759
Marcus <matthw_lew@hotmail.com>
2022-01-16 16:11:06 +0800

    init

--- quickdemo.cpp
+++ quickdemo.cpp
@@ -28,26 +28,29 @@ void QuickDemo::color_space_demo(Mat &image){
 }
 
 void QuickDemo::mat_creation_demo(Mat &image) {
-  Mat m1, m2, m3, m4;
+  Mat m1, m2, m3, m4, kernel;
 
   //----------- 用zeros创建一张 800*600 (单/3通道) 的空图片 -----------
   m1 = Mat::zeros(Size(480,320), CV_8UC(3));
   //----------- 用Scalar函数给每个像素点的每个通道负值 -----------
   m1 = Scalar(255, 0, 0);
-
+  //m2=m1，相当于m2和m1是同一个东西，改变m2就是改变m1
   m2 = m1;
   m2 = Scalar(0, 255, 0);
-
+  //clone方式相当于新生成了一个对象，m3与m1是不同的，改变m3不改变m1
   m3 = m1.clone();
   m3 = Scalar(0, 0, 255);
-
+  //copyTo的方式也是新生成一个，m4与m1也是不同的
   m1.copyTo(m4);
-  m3 = Scalar(127, 127, 127);
+  m4 = Scalar(127, 127, 127);
+
+  kernel = (Mat_<char>(3,3) << 0,-1,0,-1,5,-1,0,-1,0);
 
   imshow("m1", m1);
   imshow("m2", m2);
   imshow("m3", m3);
   imshow("m4", m4);
+  imshow("kernel", kernel);
 
   waitKey(0);
   destroyAllWindows();
